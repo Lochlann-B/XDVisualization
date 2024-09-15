e321979bdb81d41deb2873deb0c9c874e45269ee
Locky Baker <locky_baker@hotmail.co.uk>
2024-09-15 23:42:19 +0100

    3rd try's the charm

--- Render/Textures/graph-texture.js
+++ Render/Textures/graph-texture.js
@@ -2,7 +2,7 @@ import { loadTexture } from './texture-controller.js';
 
 function getGraphTexture(gl) {
     // Load texture
-    const texture = loadTexture(gl, "/Resources/grid.png");
+    const texture = loadTexture(gl, "/XDVisualization/Resources/grid.png");
     
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 
